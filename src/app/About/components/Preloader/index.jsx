e09847658974607e9b1e9f9d055e55338182b0b7
Patryck Cardozo <patryck-cardozo@hotmail.com>
2024-06-26 19:20:19 -0300

    Testes

--- src/app/About/components/Preloader/index.jsx
+++ src/app/About/components/Preloader/index.jsx
@@ -21,17 +21,17 @@ export default function Index() {
         }, index == 0 ? 1000 : 150)
     }, [index])
 
-    const initialPath = `M0 0 L${dimension.width} 0 L${dimension.width} ${dimension.height} Q${dimension.width/2} ${dimension.height + 300} 0 ${dimension.height}  L0 0`
+    const initialPath = `M0 0 L${dimension.width} 0 L${dimension.width} ${dimension.height} Q${dimension.width/2} ${dimension.height + 1000} 0 ${dimension.height}  L0 0`
     const targetPath = `M0 0 L${dimension.width} 0 L${dimension.width} ${dimension.height} Q${dimension.width/2} ${dimension.height} 0 ${dimension.height}  L0 0`
 
     const curve = {
         initial: {
             d: initialPath,
-            transition: {duration: 2, ease: [0.76, 0, 0.24, 1]}
+            transition: {duration: 0.7, ease: [0.76, 0, 0.24, 1]}
         },
         exit: {
             d: targetPath,
-            transition: {duration: 2, ease: [0.76, 0, 0.24, 1], delay: 0.3}
+            transition: {duration: 0.7, ease: [0.76, 0, 0.24, 1], delay: 0.3}
         }
     }
 
