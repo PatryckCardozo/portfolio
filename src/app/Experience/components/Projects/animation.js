d7962d9ede13099968f83e248ca403a7c9141da1
Patryck Cardozo <patryck-cardozo@hotmail.com>
2024-06-27 17:49:21 -0300

    Ajustes

---
+++ src/app/Experience/components/Projects/animation.js
@@ -0,0 +1,23 @@
+export const slideUp = {
+    initial: {
+        y: 300
+    },
+    enter: {
+        y: 0,
+        transition: {duration: 0.6, ease: [0.33, 1, 0.68, 1], delay: 2.5}
+    }
+}
+
+export const opacity = {
+    initial: {
+        opacity: 0
+    },
+    open: {
+        opacity: 1,
+        transition: {duration: 0.5}
+    },
+    closed: {
+        opacity: 0,
+        transition: {duration: 0.5}
+    }
+}
